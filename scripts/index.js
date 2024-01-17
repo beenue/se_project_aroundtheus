68bf29767ec7f08d66aa0a570793ff18b5c70079
Taylor Soffe <tjsoffe@gmail.com>
2024-01-17 16:30:57 -0700

    linked js to html via src

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,29 @@
 const initialCards = [object1, object2, object3, object4, object5, object6];
 const object1 = {
-  name ="Yosemite Valley",
-  link = "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg"
-}
+  name: "Yosemite Valley",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+};
 const object2 = {
-  name = "Lake Louise",
-  link = "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg"
-}
+  name: "Lake Louise",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+};
 
 const object3 = {
-  name = "Bald Mountains",
-  link = "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg"
-}
+  name: "Bald Mountains",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+};
 
 const object4 = {
-  name = "Latemar",
-  link = "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg"
-}
+  name: "Latemar",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+};
 
 const object5 = {
-  name = "Vanoise National Park",
-  link = "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg"
-}
+  name: "Vanoise National Park",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+};
 
 const object6 = {
-  name = "Lago di Braies",
-  link = "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg"
-}
-
-console.log("hello world");
-
-
+  name: "Lago di Braies",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+};
